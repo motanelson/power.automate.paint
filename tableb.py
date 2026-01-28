c6fd2444e73cb329c4b28c1a0104d4d1a1288845
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-28 19:56:37 +0000

    Add files via upload

--- tableb.py
+++ tableb.py
@@ -1,22 +1,18 @@
-import random
-def returning():
-    a=random.random()*15
-    a=int(a)
-    a=chr(a).encode()
-    
-    return a
 x=200
 y=200
+c=0
 outs="bitmap.dat"
 print( "\n\033c\033[40;37m\ngive the x value of the table ? ")
-#x=input()
+x=int(input())
 print( "\n\033[40;37m\ngive the y value of the table ? ")
-#y=input()
-
+y=int(input())
+print( "\n\033[40;37m\ngive the color fro 0 to 15 ? ")
+c=int(input())
+c=chr(c)
 f1=open(outs,"wb")
 for yy in range(y):
     
     for xx in range(x):
-        f1.write(returning())
+        f1.write(c.encode())
     f1.write(chr(32).encode())
 f1.close()
